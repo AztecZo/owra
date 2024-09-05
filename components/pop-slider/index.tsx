1d2d87ad30e079865665339c807527a4f0027e6a
armagansenol <armagansnl@gmail.com>
2024-09-05 19:50:45 +0300

    test roughness

--- components/pop-slider/index.tsx
+++ components/pop-slider/index.tsx
@@ -403,7 +403,7 @@ function Cup(nodes: GLTFResult["nodes"]) {
       resolution: { value: 512, min: 256, max: 2048, step: 256 },
       backsideResolution: { value: 512, min: 32, max: 2048, step: 256 },
       transmission: { value: 1, min: 0, max: 1 },
-      roughness: { value: 0.0, min: 0, max: 1, step: 0.01 },
+      roughness: { value: 0.3, min: 0, max: 1, step: 0.01 },
       ior: { value: 1.5, min: 1, max: 5, step: 0.01 },
       thickness: { value: 0.25, min: 0, max: 10, step: 0.01 },
       chromaticAberration: { value: 0.4, min: 0, max: 1 },
