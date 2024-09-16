4881af85a8d62735e83ac4a4db8e96eaa863d28c
armagansenol <armagansnl@gmail.com>
2024-09-16 13:29:06 +0300

    test

--- components/pop-slider/index.tsx
+++ components/pop-slider/index.tsx
@@ -121,7 +121,7 @@ export default function PopSlider() {
 
 function Scene() {
   return (
-    <Canvas dpr={1}>
+    <Canvas dpr={2}>
       <Suspense fallback={<LoadingScreen />}>
         <color attach="background" args={["#ffffff"]} />
 
